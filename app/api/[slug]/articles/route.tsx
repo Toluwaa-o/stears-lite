eada3120663095f1101caa12e78705e8f5872b60
Toluwalashe <asheogunleye02@gmail.com>
2025-05-22 03:27:19 +0100

    eslint fix

--- app/api/[slug]/articles/route.tsx
+++ app/api/[slug]/articles/route.tsx
@@ -1,15 +1,20 @@
-// import connectDB from "@/lib/mongodb";
-// import fetchGoogleNews from "@/utils/FetchArticles";
-// import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/lib/mongodb";
+import fetchGoogleNews from "@/utils/FetchArticles";
+import { NextRequest, NextResponse } from "next/server";
 
-// export const GET = async (request: NextRequest, { params }: { params: { slug: string } }): Promise<NextResponse> => {
-//   await connectDB;
-//   const { slug } = params;
-//   console.log(slug);
+export async function GET(
+  request: NextRequest,
+  context: { params: { slug: string } }  // Correct type for second argument
+): Promise<NextResponse> {
+  await connectDB;
 
-//   // Use request for something meaningful
-//   const headers = request.headers;
-//   console.log(headers)
-//   const articles = await fetchGoogleNews('opay');
-//   return NextResponse.json({ articles, success: true }, { status: 200 });
-// };
+  // Access params from context
+  const { slug } = context.params;
+  console.log(slug);
+
+  // Use request for something meaningful
+  const headers = request.headers;
+  console.log(headers)
+  const articles = await fetchGoogleNews('opay');
+  return NextResponse.json({ articles, success: true }, { status: 200 });
+};
