ffb0b5fd8d873ac5cdf63980343ead3a12d04714
Toluwalashe <asheogunleye02@gmail.com>
2025-05-22 03:21:10 +0100

    eslint fix

--- app/api/[slug]/articles/route.tsx
+++ app/api/[slug]/articles/route.tsx
@@ -1,15 +1,15 @@
-import connectDB from "@/lib/mongodb";
-import fetchGoogleNews from "@/utils/FetchArticles";
-import { NextRequest, NextResponse } from "next/server";
+// import connectDB from "@/lib/mongodb";
+// import fetchGoogleNews from "@/utils/FetchArticles";
+// import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (request: NextRequest, { params }: { params: { slug: string } }): Promise<NextResponse> => {
-  await connectDB;
-  const { slug } = params;
-  console.log(slug);
+// export const GET = async (request: NextRequest, { params }: { params: { slug: string } }): Promise<NextResponse> => {
+//   await connectDB;
+//   const { slug } = params;
+//   console.log(slug);
 
-  // Use request for something meaningful
-  const headers = request.headers;
-  console.log(headers)
-  const articles = await fetchGoogleNews('opay');
-  return NextResponse.json({ articles, success: true }, { status: 200 });
-};
+//   // Use request for something meaningful
+//   const headers = request.headers;
+//   console.log(headers)
+//   const articles = await fetchGoogleNews('opay');
+//   return NextResponse.json({ articles, success: true }, { status: 200 });
+// };
