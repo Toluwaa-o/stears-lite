92c323b8361cd2e154fe9aaa1368a2133a19af4b
Toluwalashe <asheogunleye02@gmail.com>
2025-05-22 03:15:41 +0100

    route eslint fix

--- app/api/[slug]/articles/route.tsx
+++ app/api/[slug]/articles/route.tsx
@@ -1,18 +1,15 @@
 import connectDB from "@/lib/mongodb";
-// import { CompanyDataModel } from "@/models/CompanyData";
 import fetchGoogleNews from "@/utils/FetchArticles";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async ({ params }: { params: Promise<{ slug: string }> }): Promise<NextResponse> => {
+export const GET = async (request: NextRequest, { params }: { params: { slug: string } }): Promise<NextResponse> => {
   await connectDB;
-  const { slug } = await params
-  console.log(slug)
-  // const company = await CompanyDataModel.findById(slug).select('company')
-  // if (!company) {
-  //   throw Error("Could not find company in database")
-  // }
+  const { slug } = params;
+  console.log(slug);
 
-  const articles = await fetchGoogleNews('opay')
-
+  // Use request for something meaningful
+  const headers = request.headers;
+  console.log(headers)
+  const articles = await fetchGoogleNews('opay');
   return NextResponse.json({ articles, success: true }, { status: 200 });
 };
