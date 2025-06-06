5cf723ec8383d04c051edacace807dd954944fbf
Toluwalashe <asheogunleye02@gmail.com>
2025-06-06 23:16:55 +0100

    adding percentage

--- app/information/components/KeyMetrics.tsx
+++ app/information/components/KeyMetrics.tsx
@@ -173,7 +173,7 @@ const KeyMetrics: React.FC<KeyMetricsProps> = ({ metrics, employeeCount, competi
                                             <path fillRule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                         </svg>
                                     )}
-                                    {growthValue || 'N/A'}
+                                    {growthValue || 'N/A'} %
                                 </span>
                             )}
                         </div>
